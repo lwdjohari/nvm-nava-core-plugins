6351e84559a0fa27197f51d1f57ef663a46e0c4e
lwdjohari <linggawasistha.djohari@outlook.com>
2024-01-22 15:35:23 +0800

    Add CMake static lib cxx17 project for core header & plugin interface.

---
+++ inc/nava/core/plugins/utils.h
@@ -0,0 +1,19 @@
+#ifndef NAVA_CORE_PLUGINS_V2_UTILS_H
+#define NAVA_CORE_PLUGINS_V2_UTILS_H
+
+namespace nvm {
+namespace nava {
+namespace core {
+namespace plugins {
+
+class Utils {
+ private:
+ public:
+  Utils();
+  ~Utils();
+};
+}  // namespace plugins
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
+#endif
---
+++ src/nava/core/plugins/utils.cc
@@ -0,0 +1,16 @@
+#include "nava/core/plugins/utils.h"
+
+namespace nvm {
+namespace nava {
+namespace core {
+namespace plugins
+{
+    
+Utils::Utils() {}
+
+Utils::~Utils() {}
+
+} // namespace plugins
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
